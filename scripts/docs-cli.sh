4bf6f90afbcdaa0000e6fb7ba2d99a2f9350afcd
Jordan Cartwright <12487178+Jordan-Cartwright@users.noreply.github.com>
2023-06-08 19:21:57 -0400

    add documentation build scripts

---
+++ scripts/docs-cli.sh
@@ -0,0 +1,432 @@
+#!/bin/bash
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+source ${ROOT_DIR}/scripts/common.sh
+
+# Defaults
+TUGBOAT_PATH=${TUGBOAT_PATH}
+DOC_TEMPLATE_DIR="docs"
+DOCS_CLI_DIR="${DOC_TEMPLATE_DIR}/cli"
+GENERATED_DOCS_DIR="generated"
+GENERATED_DOCS_CLI_DIR="${GENERATED_DOCS_DIR}/cli"
+
+# This controls what commands are documented. The order matters and shows how they will be
+# displayed by the website. 'root' is special and means that the root command page will be generated.
+# for nested commands use a '-' separator to indicate the parent command and sub command.
+DOCUMENTED_COMMANDS="root build tag manifest manifest-create"
+
+usage() {
+  echo -e "A script to generate the tugboat cli command usage documentation \n\n"
+  echo "Usage:"
+  echo "${0} [OPTIONS]"
+  echo ""
+  echo "Options:"
+  echo "    --debug            Output more information about the execution"
+  echo "    --dry-run          Print out what will happen, do not execute"
+  echo "-h, --help             Show this help message"
+  echo "-p, --path             A local path to tugboat source code that will be run with go (requires installation of golang)"
+  echo ""
+}
+
+# Generate a markdown table given a cli flag section as a string
+# i.e.
+#   -c, --config string             Custom path to a configuration file (optional)
+#       --docker-registry string    The docker registry to use (default "docker.io")
+create_markdown_table() {
+  local table="|     Option      | Default | Description |"
+  table="${table}\n| --------------- | ------- | ----------- |"
+
+  while read -r line; do
+      name_shorthand=$(echo "$line" | awk '{print $1;}')
+      default_value="$(echo "$line" | awk -F '[()]' '{print $2;}' | awk -F '[""]' '{print $2;}')"
+      description=$(echo "$line" | awk '{$1=$2=""; print $0;}')
+
+      # handle the shortname lines
+      if [[ "${name_shorthand:0-1}" == "," ]]; then
+        name_shorthand=$(echo "$line" | awk '{print $1,$2;}')
+      fi
+      # replace the default comment with nothing
+      description=$(echo "$description" | sed 's/ (default .*//')
+      # clean the description of leading words
+      description="$(echo "$description" | sed 's/strings//g; s/string//g; s/bool//g')"
+      description=$(trim "${description}")
+      # append to the table
+      table="${table}\n| $name_shorthand | ${default_value} | $description |"
+  done <<< "$1"
+  echo "${table}"
+}
+
+template_root_command() {
+  local file=$1
+  local command_output=$2
+  local available_commands=$(echo "$command_output" | sed -n '/Available Commands/,/Flags/p' | sed '/^Available Commands:/d;/^Flags:/d')
+  local see_also="## See also\n"
+
+  # loop over each line and create the see also links
+  while read -r line; do
+      cmd_name=$(echo "$line" | awk '{print $1;}')
+
+      if [[ "${cmd_name}" == "help" ]]; then
+        log_debug "see also section: skipping help command"
+        continue
+      fi
+
+      description=$(echo "$line" | awk '{ s = ""; for (i = 2; i <= NF; i++) s = s $i " "; print s }')
+      description=$(trim "${description}")
+      
+      # append to section
+      if ! [[ "${cmd_name}" =~ ^(completion|version)$ ]]; then
+        see_also="${see_also}- [${cmd_name}]({{< relref \"tugboat-${cmd_name}\">}}) - ${description}\n"
+      fi
+  done <<< "$available_commands"
+
+  if ! sed -i '' "s/__SEE_ALSO_SECTION__/${see_also}/g" "${file}"; then
+    exit 1
+  fi
+}
+
+execute_tugboat_command() {
+  # Check that at least one argument was passed
+  if [ $# -eq 0 ]; then
+    echo "Usage: execute_tugboat_command [<arg1> <arg2> ...]"
+    return 1
+  fi
+
+  local cmd=($@)
+
+  if [[ -n ${TUGBOAT_PATH} ]]; then
+    # Use local path to run the tugboat source code directly
+    cd "${TUGBOAT_PATH}"
+    local cmd_output=$(go run cmd/tugboat/main.go "${cmd[@]}" 2>&1)
+    cd "${ROOT_DIR}"
+  else
+    # Assume tugboat is installed
+    local cmd_output=$(tugboat "${cmd[@]}" 2>&1)
+  fi
+  
+  echo "${cmd_output}"
+}
+
+get_parent_cmd_description() {
+  # Check that at least one argument was passed
+  if [ $# -eq 0 ]; then
+    local parent_cmd=""
+  else 
+    local parent_cmd=$1
+  fi
+  local description=""
+
+  local cmd_output=$(execute_tugboat_command ${parent_cmd} --help)
+  description="$(echo "${cmd_output}" | head -n 1)"
+
+  echo "${description}"
+}
+
+check_requirements() {
+  log_debug "tugboat path: '${TUGBOAT_PATH}'"
+  log_debug "tugboat binary: '$(which tugboat)'"
+
+  if [[ -z ${TUGBOAT_PATH} ]] && ! check_command tugboat; then
+    log_err "Unable to generate the cli documentation, no set TUGBOAT_PATH or tugboat command found"
+    exit 1
+  fi
+
+  if [[ -n ${TUGBOAT_PATH} ]] && ! check_command go; then
+    log_err "Unable to generate the cli documentation, no installation of golang found"
+    exit 1
+  fi
+}
+
+clean_generated_content() {
+  # remove the old generated cli docs
+  if [[ -d "${GENERATED_DOCS_DIR}" ]]; then
+    log_info "Cleaning generated cli folder (${GENERATED_DOCS_CLI_DIR})"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      rm -rf ${ROOT_DIR}/${GENERATED_DOCS_CLI_DIR}/*
+    fi
+  fi
+
+  # create a fresh generated folder if needed
+  if [[ ! -d "${GENERATED_DOCS_DIR}" ]]; then
+    log_debug "Creating generated docs folder (./${GENERATED_DOCS_DIR})"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      mkdir -p "${GENERATED_DOCS_DIR}"
+    fi
+  fi
+}
+
+move_compiled_documentation() {
+  if [[ ! -d ${GENERATED_DOCS_CLI_DIR} ]]; then
+    log_debug "Creating directory: ${GENERATED_DOCS_CLI_DIR}"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      mkdir -p "${GENERATED_DOCS_CLI_DIR}"
+    fi
+  fi
+
+  log_info "Moving compiled cli documentation to: ${GENERATED_DOCS_CLI_DIR}"
+
+  # move compiled documentation to the docs root
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    cp ${DOCS_CLI_DIR}/_index.md ${GENERATED_DOCS_CLI_DIR}
+    mv ${DOCS_CLI_DIR}/tugboat*.md ${GENERATED_DOCS_CLI_DIR}
+  fi
+}
+
+generate_documentation() {
+  # Start to compile the command line interface documentation
+  value="$(find ${DOCS_CLI_DIR} -type f -name "tugboat*.md" | wc -l | sed 's/^[ ]*//')"
+  if [ "${value}" != "0" ]; then
+    log_info "Removing existing compiled cli docs"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      rm -r ${DOCS_CLI_DIR}/tugboat*.md
+    fi
+  fi
+
+  log_info "Generating documentation pages for each cli command"
+
+  command_weight=340
+  for cmd in $DOCUMENTED_COMMANDS; do
+    # split the command by - to detect sub commands
+    IFS="-" read -ra cmds <<< "$cmd"
+    command=${cmds[0]}
+    sub_command=${cmds[1]}
+
+    options="${command}"
+    cmd_title="$(echo $cmd | cut -d "-" -f 1)"
+    if [[ -n "${sub_command}" ]]; then
+      log_debug "there is a sub command: ${sub_command}"
+      options="${command} ${sub_command}"
+      cmd_title="$(echo $cmd | cut -d "-" -f 1) $(echo $cmd | cut -d "-" -f 2)"
+    fi
+
+    if [[ "$cmd" == "root" ]]; then
+      doc_tile="tugboat"
+      options=""
+    else
+      doc_tile="tugboat ${cmd_title}"
+    fi
+
+    log_info "Generating page for '${doc_tile}'"
+
+    log_debug "Command to run: '${options}'"
+
+    # run the command and capture output
+    cmd_output=$(execute_tugboat_command ${options} --help)
+    log_debug "Command output:"
+    log_debug "$cmd_output" # this will preserve new lines
+
+    if [[ ${cmd_output} == *"not found"* || ${cmd_output} == *"unknown command"* ]]; then
+      log_err "there was an issue building the docs for '${doc_tile}'"
+      continue
+    fi
+
+    doc_desc="$(echo "${cmd_output}" | head -n 1)"
+    
+    has_commands=$(echo "$cmd_output" | grep "Available Commands:" | wc -l | sed 's/^[ ]*//')
+    if [[ "${has_commands}" == 0 ]]; then
+      # parse the output to Usage: ... Flags: then remove the lines that start with 'Usage:' and 'Flags:'
+      usage_section=$(echo "$cmd_output" | sed -n '/Usage/,/Flags/p' | sed '/^Usage:/d;/^Flags:/d')
+    else
+      # parse the sub command output between Usage: ... Available Commands:
+      usage_section=$(echo "$cmd_output" | sed -n '/Usage/,/Available Commands/p' | sed '/^Usage:/d;/^Available Commands:/d')
+    fi
+
+    usage_section=$(trim_left "${usage_section}")
+    # count if tugboat is seen 2 times
+    word="tugboat"
+    count=$(echo "$usage_section" | tr -d '[[:space:]]' | grep -o "$word" | wc -l)
+    count=$(trim_left "${count}")
+    # find the index of the second word
+    if [[ "${count}" != "1" ]]; then
+      # pick the first command to show in the docs
+      usage_section="$(echo "${usage_section}" | head -n 1)"
+    fi
+
+    doc_usage="${usage_section}"
+    log_debug "Doc usage: \"${doc_usage}\""
+
+    options_section=$(echo "$cmd_output" | sed -n '/Flags/,/Global Flags/p' | sed '/^Flags:/d;/^Global Flags:/d;/^Use/d')
+
+    if [[ "${IS_DEBUG}" == "true" ]]; then
+      printf "${DIM}"
+      echo "=========== options for '${options}' ==========="
+      echo "${options_section}"
+      echo "================================================"
+      printf "${RESET}"
+    fi
+
+    log_debug "Creating table for options for '${options}'"
+    doc_command_options=$(create_markdown_table "$options_section")
+    # replace / with \/
+    doc_command_options=$(echo "${doc_command_options}" | sed 's:/:\\/:g')
+
+    if [[ "${IS_DEBUG}" == "true" ]]; then
+      printf "${DIM}"
+      echo "=========== Generated Options Table ==========="
+      echo "${doc_command_options}"
+      echo "==============================================="
+      printf "${RESET}"
+    fi
+
+    options_section=$(echo "$cmd_output" | sed -n '/Global Flags/,$p' | sed '/^Global Flags:/d')
+    doc_global_options="$(create_markdown_table "$options_section")"
+    
+    # get parent command information
+    if [[ -n "${sub_command}" ]]; then
+      # this is a sub-command
+      doc_parent_cmd="${command}"
+      doc_parent_ref="tugboat-${command}"
+      doc_parent_cmd_desc="$(get_parent_cmd_description ${doc_parent_cmd})"
+    else
+      # this is a not sub-command
+      doc_parent_cmd="tugboat"
+      doc_parent_ref="tugboat"
+      doc_parent_cmd_desc="$(get_parent_cmd_description)"
+    fi
+
+    # copy the template to the new file name
+    filename="${DOCS_CLI_DIR}/tugboat-${cmd}.md"
+    template_file="${DOCS_CLI_DIR}/template.md"
+    if [[ "$cmd" == "root" ]]; then
+      filename="${DOCS_CLI_DIR}/tugboat.md"
+      template_file="${DOCS_CLI_DIR}/template-root.md"
+    fi
+    
+    log_debug "Copying template file (${template_file} -> ${filename})"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      cp ${template_file} ${filename}
+    fi
+
+    if [[ "$cmd" == "root" ]]; then
+      log_debug "Making root command template"
+      if [[ "${IS_DRY_RUN}" != "true" ]]; then
+        template_root_command "${filename}" "${cmd_output}"
+      fi
+    fi
+
+    # Replace template values
+    log_debug "Replace template placeholders with processed values"
+
+    log_debug "Updating document draft status"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/draft: true/draft: false/g" "$filename"; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document weight"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/weight: 140/weight: ${command_weight}/g" "$filename"; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document title"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/__TITLE__/${doc_tile}/g" $filename; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document short description"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/__COBRA_SHORT_DESCRIPTION__/${doc_desc}/g" $filename; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document usage"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/__TUGBOAT_USAGE__/${doc_usage}/g" $filename; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document command options"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/__TUGBOAT_COMMAND_OPTIONS__/${doc_command_options}/g" $filename; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document global options"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/__TUGBOAT_GLOBAL_OPTIONS__/${doc_global_options}/g" $filename; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document parent command"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/__PARENT_COMMAND__/${doc_parent_cmd}/g" $filename; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document parent ref"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/__PARENT_COMMAND_REF__/${doc_parent_ref}/g" $filename; then
+        exit 1
+      fi
+    fi
+
+    log_debug "Updating document parent description"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      if ! sed -i '' "s/__PARENT_COMMAND_DESCRIPTION__/${doc_parent_cmd_desc}/g" $filename; then
+        exit 1
+      fi
+    fi
+
+    # increase the command document weight
+    command_weight=$((command_weight+1))
+  done
+
+  move_compiled_documentation
+
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    log_info "The cli command usage documentation was generated successfully"
+  fi
+}
+
+main() {
+  # Parse options
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+      -h|--help)
+      usage
+      exit 0
+      ;;
+      --debug)
+      IS_DEBUG=true
+      ;;
+      --dry-run)
+      IS_DRY_RUN=true
+      ;;
+      -p|--path)
+      TUGBOAT_PATH=$2
+      shift
+      ;;
+      *)
+      echo "Unknown option: $key"
+      usage
+      exit 1
+      ;;
+    esac
+    shift
+  done
+
+  setup_color
+
+  if [[ "${IS_DRY_RUN}" == "true" ]]; then
+    log_warn "Dry run in progress"
+  fi
+
+  check_requirements
+  clean_generated_content
+  generate_documentation
+
+}
+
+main "$@"
---
+++ scripts/docs-generate.sh
@@ -0,0 +1,299 @@
+#!/bin/bash
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+source ${ROOT_DIR}/scripts/common.sh
+
+# Defaults
+CONTENT_DOCS_DIR="content/docs"
+DOC_TEMPLATE_DIR="docs"
+GENERATED_DOCS_DIR="generated"
+REPOSITORY="gotugboat/tugboat"
+REPO_COPY_DIR="repos/tugboat"
+
+usage() {
+  echo -e "A script to generate the tugboat documentation \n\n"
+  echo "Usage:"
+  echo "${0} [OPTIONS]"
+  echo ""
+  echo "Options:"
+  echo "    --debug            Output more information about the execution"
+  echo "    --dry-run          Print out what will happen, do not execute"
+  echo "-h, --help             Show this help message"
+  echo "-p, --path             A local path to tugboat source code that will be run with go (requires installation of golang)"
+  echo "    --skip-cli         Skip generating the command line interface documentation"
+  echo ""
+}
+
+clean_generated_content() {
+  # remove the old generated docs
+  if [[ -d "${GENERATED_DOCS_DIR}" ]]; then
+    log_info "Cleaning contents of the generated cli folder (./${GENERATED_DOCS_DIR})"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      rm -rf ${GENERATED_DOCS_DIR}/*
+    fi
+  fi
+
+  # create a fresh generated folder if needed
+  if [[ ! -d "${GENERATED_DOCS_DIR}" ]]; then
+    log_debug "Creating generated docs folder (./${GENERATED_DOCS_DIR})"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      mkdir -p "${GENERATED_DOCS_DIR}"
+    fi
+  fi
+}
+
+# clones the tugboat repository locally
+clone_project_repository() {
+  log_info "Cloning project repository (${REPOSITORY}) to ${REPO_COPY_DIR}"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    local tag=$(cat ${ROOT_DIR}/data/tugboat.yml | grep version | awk -F ': ' '{print $2}')
+    
+    log_debug "checking out the ${tag} branch"
+    
+    if ! git clone --branch "${tag}" "https://github.com/${REPOSITORY}.git" "${REPO_COPY_DIR}" ; then
+      log_err "Cloning the ${REPOSITORY} repository failed"
+      exit 1
+    fi
+  fi
+}
+
+generate_documentation() {
+  log_info "Generating the documentation"
+
+  # move document sections into the docs folder
+  log_info "Copying ${DOC_TEMPLATE_DIR}/_index.md to ${GENERATED_DOCS_DIR}"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    cp "${DOC_TEMPLATE_DIR}/_index.md" "${GENERATED_DOCS_DIR}"
+  fi
+
+  log_info "Preparing getting started documentation"
+
+  if [[ ! -d ${GENERATED_DOCS_DIR}/getting-started ]]; then
+    log_debug "Creating directory: ${GENERATED_DOCS_DIR}/getting-started"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      mkdir -p "${GENERATED_DOCS_DIR}/getting-started"
+    fi
+  fi
+
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    log_debug "Copying getting started documentation (${DOC_TEMPLATE_DIR}/getting-started -> ${GENERATED_DOCS_DIR})"
+    cp -r "${DOC_TEMPLATE_DIR}/getting-started" "${GENERATED_DOCS_DIR}"
+  fi
+
+  log_debug "Setting install version number"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    tugboat_docs_version=$(cat ${ROOT_DIR}/data/tugboat.yml | grep version | awk -F ': ' '{print $2}' | sed 's/^.//')
+    if ! sed -i '' "s/__TUGBOAT_VERSION__/${tugboat_docs_version//$'\n'/\\n}/g"  ${GENERATED_DOCS_DIR}/getting-started/installation.md; then
+      exit 1
+    fi
+  fi
+
+  log_info "Preparing configuration documentation"
+
+  if [[ ! -d ${GENERATED_DOCS_DIR}/configuration ]]; then
+    log_debug "Creating directory: ${GENERATED_DOCS_DIR}/configuration"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      mkdir -p "${GENERATED_DOCS_DIR}/configuration"
+    fi
+  fi
+
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    log_debug "Copying configuration documentation (${DOC_TEMPLATE_DIR}/configuration -> ${GENERATED_DOCS_DIR})"
+    cp -r "${DOC_TEMPLATE_DIR}/configuration" "${GENERATED_DOCS_DIR}"
+  fi
+
+  log_info "Preparing help documentation"
+
+  if [[ ! -d ${GENERATED_DOCS_DIR}/help ]]; then
+    log_debug "Creating directory: ${GENERATED_DOCS_DIR}/help"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      mkdir -p "${GENERATED_DOCS_DIR}/help"
+    fi
+  fi
+
+  log_debug "Copying configuration documentation (${DOC_TEMPLATE_DIR}/help -> ${GENERATED_DOCS_DIR})"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    cp -r "${DOC_TEMPLATE_DIR}/help" "${GENERATED_DOCS_DIR}"
+  fi
+
+  # ==================== Generate the contributing guidelines doc page ====================
+
+  log_info "Generating the contributing guidelines page"
+
+  log_debug "Setting page content"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    contribution_guidelines=$(cat ${REPO_COPY_DIR}/.github/CONTRIBUTING.md | sed 's:/:\\/:g')
+    if ! sed -i '' "s/__TUGBOAT_CONTRIBUTION_GUIDELINES__/${contribution_guidelines//$'\n'/\\n}/g"  ${GENERATED_DOCS_DIR}/help/contribution-guidelines.md; then
+      exit 1
+    fi
+  fi
+
+  # fix the markdown links to reference correctly
+  # replace ../LICENSE with https://github.com/gotugboat/tugboat/blob/main/LICENSE
+  log_debug "Replace license markdown links to reference back to github"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    license_url=$(echo github.com/${REPOSITORY}/blob/main/LICENSE | sed 's:/:\\/:g')
+    if ! sed -i '' "s/\.\.\/LICENSE/https:\/\/${license_url//$'\n'/\\n}/g"  ${GENERATED_DOCS_DIR}/help/contribution-guidelines.md; then
+      exit 1
+    fi
+  fi
+
+  # replace ./CODE_OF_CONDUCT.md with https://github.com/gotugboat/tugboat/tree/main/.github/
+  log_debug "Replace code of conduct markdown links to reference back to github"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    code_of_conduct_url="{{< relref \"code-of-conduct\" >}}"
+    if ! sed -i '' "s/\.\/CODE_OF_CONDUCT\.md/${code_of_conduct_url//$'\n'/\\n}/g"  ${GENERATED_DOCS_DIR}/help/contribution-guidelines.md; then
+      exit 1
+    fi
+  fi
+
+  log_debug "Modifying page content"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    remove_text="# Contributing"
+    if ! sed -i '' "s/${remove_text}//g"  ${GENERATED_DOCS_DIR}/help/contribution-guidelines.md; then
+      exit 1
+    fi
+  fi
+
+  # ==================== Generate the code of conduct page ====================
+
+  log_info "Generating the code of conduct page"
+
+  log_debug "Setting page content"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    code_of_conduct=$(cat ${REPO_COPY_DIR}/.github/CODE_OF_CONDUCT.md | sed 's:/:\\/:g')
+    if ! sed -i '' "s/__TUGBOAT_CODE_OF_CONDUCT__/${code_of_conduct//$'\n'/\\n}/g"  ${GENERATED_DOCS_DIR}/help/code-of-conduct.md; then
+      exit 1
+    fi
+  fi
+
+  log_debug "Modifying page content"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    remove_text="# Contributor Covenant Code of Conduct"
+    if ! sed -i '' "s/${remove_text}//g"  ${GENERATED_DOCS_DIR}/help/code-of-conduct.md; then
+      exit 1
+    fi
+  fi
+
+  # ==================== Add the example file to the documentation ====================
+
+  log_info "Adding example config file to docs"
+
+  log_debug "Setting page content"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    # replace / with \/ in the output
+    example_config=$(cat ${REPO_COPY_DIR}/example.tugboat.yaml | sed 's:/:\\/:g')
+    # this will replace the newline characters with the escape sequence
+    if ! sed -i '' "s/__EXAMPLE_TUGBOAT_FILE_CONTENT__/${example_config//$'\n'/\\n}/g" ${GENERATED_DOCS_DIR}/configuration/example-file.md; then
+      exit 1
+    fi
+  fi
+
+  # ==================== Generate the cli docs ====================
+
+  if [[ "${SKIP_CLI}" == "true" ]]; then
+    log_info "Skipping command line interface documentation" 
+  fi
+
+  if [[ "${SKIP_CLI}" != "true" ]]; then
+    log_info "Generating command line interface documentation"
+
+    gen_cli_args=""
+    if [[ "${IS_DEBUG}" == "true" ]]; then
+      gen_cli_args="${gen_cli_args} --debug"
+    fi
+
+    if [[ "${IS_DRY_RUN}" == "true" ]]; then
+      gen_cli_args="${gen_cli_args} --dry-run"
+    fi
+
+    if [[ -n "${TUGBOAT_PATH}" ]]; then
+      gen_cli_args="${gen_cli_args} --path ${TUGBOAT_PATH}"
+    fi
+
+    ./scripts/docs-cli.sh ${gen_cli_args}
+
+    rc=$?
+    if [[ "${rc}" != "0" ]]; then
+      log_err "failed to generate command line interface documentation"
+    fi
+  fi
+
+  # ==================== Move the generated documentation to the docs folder ====================
+
+  log_info "Finalizing generated documentation"
+
+  log_debug "Moving generated documentation to ${CONTENT_DOCS_DIR}"
+  # If everything so far as worked, remove the old docs
+  if [[ -d "${CONTENT_DOCS_DIR}" ]]; then
+    log_debug "Cleaning existing content docs folder"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      rm -r "${CONTENT_DOCS_DIR}"
+    fi
+  fi
+
+  # create the fresh docs folder
+  if [[ ! -d "${CONTENT_DOCS_DIR}" ]]; then
+    log_debug "Creating folder (${CONTENT_DOCS_DIR})"
+    if [[ "${IS_DRY_RUN}" != "true" ]]; then
+      mkdir -p "${CONTENT_DOCS_DIR}"
+    fi
+  fi
+
+  log_debug "Copying generated documentation to docs (${GENERATED_DOCS_DIR}/* -> ${CONTENT_DOCS_DIR})"
+  if [[ "${IS_DRY_RUN}" != "true" ]]; then
+    cp -r ${GENERATED_DOCS_DIR}/* ${CONTENT_DOCS_DIR}
+  fi
+
+  log_info "Documentation generation completed"
+}
+
+main() {
+  # Parse options
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+      -h|--help)
+      usage
+      exit 0
+      ;;
+      --debug)
+      IS_DEBUG=true
+      ;;
+      --dry-run)
+      IS_DRY_RUN=true
+      ;;
+      -p|--path)
+      TUGBOAT_PATH=$2
+      shift
+      ;;
+      --skip-cli)
+      SKIP_CLI=true
+      ;;
+      *)
+      echo "Unknown option: $key"
+      usage
+      exit 1
+      ;;
+    esac
+    shift
+  done
+
+  setup_color
+
+  if [[ "${IS_DRY_RUN}" == "true" ]]; then
+    log_warn "Dry run in progress"
+  fi
+
+  clean_generated_content
+
+  if [[ ! -d "${REPO_COPY_DIR}" ]]; then
+    clone_project_repository
+  fi
+
+  generate_documentation
+
+}
+
+main "$@"
